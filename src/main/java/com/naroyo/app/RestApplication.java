54007446a4ebf3b18bdfd83834a528eba48dc44f
naroyo <naroyo@192.168.1.10>
2018-01-22 19:17:27 +0100

    Suppression messagerestservice

--- src/main/java/com/naroyo/app/RestApplication.java
+++ src/main/java/com/naroyo/app/RestApplication.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
-import com.naroyo.rest.MessageRestService;
 import com.naroyo.rest.timesup.TimesUpRestService;
 
 public class RestApplication extends Application {
@@ -13,7 +12,6 @@ public class RestApplication extends Application {
 	private Set<Object> singletons = new HashSet<>();
 
 	public RestApplication() {
-		this.singletons.add(new MessageRestService());
 		this.singletons.add(new TimesUpRestService());
 	}
 
